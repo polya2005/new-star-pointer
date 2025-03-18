aafc702aa64722af96258c498bd17b7f4e4dbbd9
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-03-18 16:16:02 -0400

    add XPT2046 lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2025 Boonyakorn Thanpanit
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,165 +18,169 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-*/
+ */
 
-#include <Adafruit_ADXL345_U.h>
-#include <Adafruit_Sensor.h>
 #include <Arduino.h>
+#include <TFT_eSPI.h>
 #include <Wire.h>
+#include <XPT2046_Touchscreen.h>
+#include <lvgl.h>
 
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+#define XPT2046_CS 15
 
-void displaySensorDetails(void) {
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print("Sensor:       ");
-  Serial.println(sensor.name);
-  Serial.print("Driver Ver:   ");
-  Serial.println(sensor.version);
-  Serial.print("Unique ID:    ");
-  Serial.println(sensor.sensor_id);
-  Serial.print("Max Value:    ");
-  Serial.print(sensor.max_value);
-  Serial.println(" m/s^2");
-  Serial.print("Min Value:    ");
-  Serial.print(sensor.min_value);
-  Serial.println(" m/s^2");
-  Serial.print("Resolution:   ");
-  Serial.print(sensor.resolution);
-  Serial.println(" m/s^2");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
+XPT2046_Touchscreen touchscreen(XPT2046_CS);
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
+
+// Touchscreen coordinates: (x, y) and pressure (z)
+int x, y, z;
+
+#define DRAW_BUF_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
+// If logging is enabled, it will inform the user about what is happening in the
+// library
+void log_print(lv_log_level_t level, const char* buf) {
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
 }
 
-void displayDataRate(void) {
-  Serial.print("Data Rate:    ");
+// Get the Touchscreen data
+void touchscreen_read(lv_indev_t* indev, lv_indev_data_t* data) {
+  // Checks if Touchscreen was touched, and prints X, Y and Pressure (Z)
+  if (touchscreen.tirqTouched() && touchscreen.touched()) {
+    // Get Touchscreen points
+    TS_Point p = touchscreen.getPoint();
+    // Calibrate Touchscreen points with map function to the correct width and
+    // height
+    x = map(p.x, 200, 3700, 1, SCREEN_WIDTH);
+    y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
+    z = p.z;
 
-  switch (accel.getDataRate()) {
-    case ADXL345_DATARATE_3200_HZ:
-      Serial.print("3200 ");
-      break;
-    case ADXL345_DATARATE_1600_HZ:
-      Serial.print("1600 ");
-      break;
-    case ADXL345_DATARATE_800_HZ:
-      Serial.print("800 ");
-      break;
-    case ADXL345_DATARATE_400_HZ:
-      Serial.print("400 ");
-      break;
-    case ADXL345_DATARATE_200_HZ:
-      Serial.print("200 ");
-      break;
-    case ADXL345_DATARATE_100_HZ:
-      Serial.print("100 ");
-      break;
-    case ADXL345_DATARATE_50_HZ:
-      Serial.print("50 ");
-      break;
-    case ADXL345_DATARATE_25_HZ:
-      Serial.print("25 ");
-      break;
-    case ADXL345_DATARATE_12_5_HZ:
-      Serial.print("12.5 ");
-      break;
-    case ADXL345_DATARATE_6_25HZ:
-      Serial.print("6.25 ");
-      break;
-    case ADXL345_DATARATE_3_13_HZ:
-      Serial.print("3.13 ");
-      break;
-    case ADXL345_DATARATE_1_56_HZ:
-      Serial.print("1.56 ");
-      break;
-    case ADXL345_DATARATE_0_78_HZ:
-      Serial.print("0.78 ");
-      break;
-    case ADXL345_DATARATE_0_39_HZ:
-      Serial.print("0.39 ");
-      break;
-    case ADXL345_DATARATE_0_20_HZ:
-      Serial.print("0.20 ");
-      break;
-    case ADXL345_DATARATE_0_10_HZ:
-      Serial.print("0.10 ");
-      break;
-    default:
-      Serial.print("???? ");
-      break;
+    data->state = LV_INDEV_STATE_PRESSED;
+
+    // Set the coordinates
+    data->point.x = x;
+    data->point.y = y;
+
+    // Print Touchscreen info about X, Y and Pressure (Z) on the Serial Monitor
+    /* Serial.print("X = ");
+    Serial.print(x);
+    Serial.print(" | Y = ");
+    Serial.print(y);
+    Serial.print(" | Pressure = ");
+    Serial.print(z);
+    Serial.println();*/
+  } else {
+    data->state = LV_INDEV_STATE_RELEASED;
   }
-  Serial.println(" Hz");
 }
 
-void displayRange(void) {
-  Serial.print("Range:         +/- ");
-
-  switch (accel.getRange()) {
-    case ADXL345_RANGE_16_G:
-      Serial.print("16 ");
-      break;
-    case ADXL345_RANGE_8_G:
-      Serial.print("8 ");
-      break;
-    case ADXL345_RANGE_4_G:
-      Serial.print("4 ");
-      break;
-    case ADXL345_RANGE_2_G:
-      Serial.print("2 ");
-      break;
-    default:
-      Serial.print("?? ");
-      break;
+int btn1_count = 0;
+// Callback that is triggered when btn1 is clicked
+static void event_handler_btn1(lv_event_t* e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  if (code == LV_EVENT_CLICKED) {
+    btn1_count++;
+    LV_LOG_USER("Button clicked %d", (int)btn1_count);
   }
-  Serial.println(" g");
+}
+
+// Callback that is triggered when btn2 is clicked/toggled
+static void event_handler_btn2(lv_event_t* e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t* obj = reinterpret_cast<lv_obj_t*>(lv_event_get_target(e));
+  if (code == LV_EVENT_VALUE_CHANGED) {
+    LV_UNUSED(obj);
+    LV_LOG_USER("Toggled %s",
+                lv_obj_has_state(obj, LV_STATE_CHECKED) ? "on" : "off");
+  }
+}
+
+static lv_obj_t* slider_label;
+// Callback that prints the current slider value on the TFT display and Serial
+// Monitor for debugging purposes
+static void slider_event_callback(lv_event_t* e) {
+  lv_obj_t* slider = reinterpret_cast<lv_obj_t*>(lv_event_get_target(e));
+  char buf[8];
+  lv_snprintf(buf, sizeof(buf), "%d%%",
+              static_cast<int>(lv_slider_get_value(slider)));
+  lv_label_set_text(slider_label, buf);
+  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+  LV_LOG_USER("Slider changed to %d%%",
+              static_cast<int>(lv_slider_get_value(slider)));
+}
+
+void lv_create_main_gui(void) {
+  // Create a text label aligned center on top ("Hello, world!")
+  lv_obj_t* text_label = lv_label_create(lv_screen_active());
+  lv_label_set_long_mode(text_label,
+                         LV_LABEL_LONG_WRAP);  // Breaks the long lines
+  lv_label_set_text(text_label, "Hello, world!");
+  lv_obj_set_width(text_label,
+                   150);  // Set smaller width to make the lines wrap
+  lv_obj_set_style_text_align(text_label, LV_TEXT_ALIGN_CENTER, 0);
+  lv_obj_align(text_label, LV_ALIGN_CENTER, 0, -90);
+
+  lv_obj_t* btn_label;
+  // Create a Button (btn1)
+  lv_obj_t* btn1 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn1, event_handler_btn1, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -50);
+  lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
+
+  btn_label = lv_label_create(btn1);
+  lv_label_set_text(btn_label, "Button");
+  lv_obj_center(btn_label);
+
+  // Create a Toggle button (btn2)
+  lv_obj_t* btn2 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn2, event_handler_btn2, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 10);
+  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+
+  btn_label = lv_label_create(btn2);
+  lv_label_set_text(btn_label, "Toggle");
+  lv_obj_center(btn_label);
+
+  // Create a slider aligned in the center bottom of the TFT display
+  lv_obj_t* slider = lv_slider_create(lv_screen_active());
+  lv_obj_align(slider, LV_ALIGN_CENTER, 0, 60);
+  lv_obj_add_event_cb(slider, slider_event_callback, LV_EVENT_VALUE_CHANGED,
+                      NULL);
+  lv_slider_set_range(slider, 0, 100);
+  lv_obj_set_style_anim_duration(slider, 2000, 0);
+
+  // Create a label below the slider to display the current slider value
+  slider_label = lv_label_create(lv_screen_active());
+  lv_label_set_text(slider_label, "0%");
+  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
 }
 
 void setup(void) {
   Serial.begin(9600);
   delay(1000);  // delay for the serial monitor to open
-  Serial.println("Accelerometer Test");
-  Serial.println("");
+  lv_init();
+  lv_log_register_print_cb(log_print);
+  touchscreen.begin();
+  touchscreen.setRotation(0);
 
-  /* Initialise the sensor */
-  if (!accel.begin()) {
-    /* There was a problem detecting the ADXL345 ... check your connections */
-    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-    while (1) {
-    }
-  }
+  lv_display_t* disp;
+  disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf,
+                            sizeof(draw_buf));
+  lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270);
 
-  /* Set the range to whatever is appropriate for your project */
-  // accel.setRange(ADXL345_RANGE_16_G);
-  // accel.setRange(ADXL345_RANGE_8_G);
-  // accel.setRange(ADXL345_RANGE_4_G);
-  accel.setRange(ADXL345_RANGE_2_G);
+  lv_indev_t* indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  lv_indev_set_read_cb(indev, touchscreen_read);
 
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-
-  /* Display additional settings (outside the scope of sensor_t) */
-  displayDataRate();
-  displayRange();
-  Serial.println("");
+  lv_create_main_gui();
 }
 
 void loop(void) {
-  /* Get a new sensor event */
-  sensors_event_t event;
-  accel.getEvent(&event);
-
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: ");
-  Serial.print(event.acceleration.x);
-  Serial.print("  ");
-  Serial.print("Y: ");
-  Serial.print(event.acceleration.y);
-  Serial.print("  ");
-  Serial.print("Z: ");
-  Serial.print(event.acceleration.z);
-  Serial.print("  ");
-  Serial.println("m/s^2 ");
-  delay(500);
+  lv_task_handler();
+  lv_tick_inc(5);
+  delay(5);
 }
