ac3d7c1b9e16bcbdb49eb60163e6acff6f12dba7
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-03-18 17:03:59 -0400

    update docs + add gps

--- lib/Sensors/Sensors.h
+++ lib/Sensors/Sensors.h
@@ -13,15 +13,22 @@
  * @brief A class that provides access to various sensors.
  *
  * The Sensors class provides access to various sensors, such as the
- * accelerometer, magnetometer, and GPS. The class provides methods to read
- * data from the sensors and to calibrate the sensors.
+ * accelerometer, magnetometer, and GPS. Think of this class as an abstraction
+ * layer that hides the details of the sensors from the rest of the program.
+ * The class provides methods to read data from the sensors and to calibrate
+ * the sensors.
+ *
+ * The implementation assumes that the accelerometer is an ADXL345 and the
+ * magnetometer is a QMC5883. Both of those sensors are connected to the I2C
+ * bus. The GPS module is assumed to be connected to UART0.
  */
 class Sensors {
  private:
   Adafruit_ADXL345_Unified accel;
   DFRobot_QMC5883 compass;
+  SerialUART& gps;
 
-  Sensors() {}
+  Sensors() : gps(Serial1) {}
   Sensors(const Sensors&) = delete;
   void operator=(const Sensors&) = delete;
 
