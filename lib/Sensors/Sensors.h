8b71f20add2379d44facbf17aed921c4ace4d983
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-04-28 12:54:20 -0400

    add gps-related function prototypes

--- lib/Sensors/Sensors.h
+++ lib/Sensors/Sensors.h
@@ -7,6 +7,8 @@
 
 #include "Adafruit_ADXL345_U.h"
 #include "DFRobot_QMC5883.h"
+#include "Datatypes.h"
+#include "TinyGPSPlus.h"
 #include "Vectors.h"
 
 /**
@@ -20,15 +22,17 @@
  *
  * The implementation assumes that the accelerometer is an ADXL345 and the
  * magnetometer is a QMC5883. Both of those sensors are connected to the I2C
- * bus. The GPS module is assumed to be connected to UART0.
+ * bus. The GPS module is assumed to be connected to UART0 but can be
+ * customized in Init().
  */
 class Sensors {
  private:
   Adafruit_ADXL345_Unified accel;
   DFRobot_QMC5883 compass;
-  SerialUART& gps;
+  SerialUART& gpsSerial;
+  TinyGPSPlus gps;
 
-  Sensors() : gps(Serial1) {}
+  Sensors() : gpsSerial(Serial1) {}
   Sensors(const Sensors&) = delete;
   void operator=(const Sensors&) = delete;
 
@@ -44,7 +48,7 @@ class Sensors {
    * @brief Initializes the sensors.
    *
    */
-  void Init();
+  void Init(SerialUART* gpsSerial = &Serial1);
 
   /**
    * @brief Reads the acceleration from the accelerometer.
@@ -57,10 +61,34 @@ class Sensors {
 
   /**
    * @brief Reads the magnetic field from the magnetometer.
+   * Reads the magnetic field from the magnetometer and returns it as a 3d
+   * vector. Must call Init() before calling this method for the first time.
    *
    * @return Vector3 The magnetic field vector.
    */
   Vector3 ReadMagneticField();
+
+  /**
+   * @brief Reads the observer's location from the GPS.
+   *
+   * Reads the observer's location from the GPS and returns it as an
+   * ObserverLocation struct. Must call Init() before calling this method for
+   * the first time.
+   * 
+   * @param timeout The timeout in milliseconds. Default is 2000ms.
+   *
+   * @return ObserverLocation The observer's location.
+   */
+  ObserverLocation ReadObserverLocation(uint32_t timeout = 2000ul);
+
+  /**
+   * @brief Reads the current Julian date from the GPS.
+   * 
+   * Reads the current Julian date from the GPS and returns it as a double.
+   * 
+   * @return double The current Julian date.
+   */
+  double ReadJulianDate();
 };
 
 #endif  // LIB_SENSORS_SENSORS_H_
