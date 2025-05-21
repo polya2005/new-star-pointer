def7f6826e8fd341b7bdde1d849cf8ada1b2441b
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-05-21 00:58:36 -0400

    cross product and normalization

--- lib/Vectors/Vectors.h
+++ lib/Vectors/Vectors.h
@@ -7,9 +7,10 @@
 
 /**
  * @brief A 3d vector.
- * 
+ *
  */
-typedef struct {
+class Vector3 {
+ public:
   /**
    * @brief The x component of the vector.
    */
@@ -24,6 +25,40 @@ typedef struct {
    * @brief The z component of the vector.
    */
   double z;
-} Vector3;
+
+  /**
+   * @brief Computes the cross product of this vector and another vector.
+   *
+   * @param v The other vector.
+   * @return Vector3 The cross product of this vector and the other vector.
+   */
+  Vector3 Cross(const Vector3& v) const;
+
+  /**
+   * @brief Computes the magnitude of the vector.
+   *
+   * @return double The magnitude of the vector.
+   */
+  double Magnitude() const;
+
+  /**
+   * @brief Computes the normalized vector.
+   *
+   * Normalizes the vector and returns a new vector with the same direction.
+   * Does not modify the original vector.
+   *
+   * @return Vector3 The normalized vector.
+   * @see Normalize()
+   */
+  Vector3 Normalized() const;
+
+  /**
+   * @brief Normalizes the vector.
+   *
+   * Normalizes the vector in place.
+   *
+   */
+  void Normalize();
+};
 
 #endif  // LIB_VECTORS_VECTORS_H_
