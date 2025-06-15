2ef9667665469bd35e8f5658920edbc9537f9d95
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-06-15 17:27:09 +0700

    refactor

---
+++ include/ui/setup.h
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2025 Boonyakorn Thanpanit
+ */
+
+#ifndef UI_SETUP_H_
+#define UI_SETUP_H_
+
+void setup_screen();
+
+#endif  // UI_SETUP_H_
---
+++ src/setup.cpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2025 Boonyakorn Thanpanit
+ */
+#include "ui/setup.h"
+
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+#include <lvgl.h>
+
+#define XPT2046_CS 15
+
+XPT2046_Touchscreen touchscreen(XPT2046_CS);
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
+
+// Touchscreen coordinates: (x, y) and pressure (z)
+static int x, y, z;
+
+static constexpr int DRAW_BUF_SIZE =
+    (SCREEN_WIDTH * SCREEN_HEIGHT / 10 * (LV_COLOR_DEPTH / 8));
+static uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
+void log_print(lv_log_level_t level, const char* buf) {
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
+}
+
+void touchscreen_read(lv_indev_t* indev, lv_indev_data_t* data) {
+  // Checks if Touchscreen was touched, and prints X, Y and Pressure (Z)
+  if (touchscreen.tirqTouched() && touchscreen.touched()) {
+    // Get Touchscreen points
+    TS_Point p = touchscreen.getPoint();
+    // Calibrate Touchscreen points with map function to the correct width and
+    // height
+    x = map(p.x, 200, 3700, 1, SCREEN_WIDTH);
+    y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
+    z = p.z;
+
+    data->state = LV_INDEV_STATE_PRESSED;
+
+    // Set the coordinates
+    data->point.x = x;
+    data->point.y = y;
+  } else {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
+void setup_screen() {
+  lv_init();
+  lv_log_register_print_cb(log_print);
+  touchscreen.begin();
+  touchscreen.setRotation(0);
+
+  lv_display_t* disp;
+  disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf,
+                            sizeof(draw_buf));
+  lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270);
+
+  lv_indev_t* indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  lv_indev_set_read_cb(indev, touchscreen_read);
+}
