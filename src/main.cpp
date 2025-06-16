97879d286990074f0c89dceb238dfca57b7848b1
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-06-16 15:27:31 +0700

    setup identify tab

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,65 @@
  * SOFTWARE.
  */
 
+#define DEBUG
+
 #include <Arduino.h>
+#include <MovementController.h>
+#include <TempDebug.h>
 #include <Wire.h>
 #include <lvgl.h>
 
-#include "ui/setup.h"
+#include "ui.h"
+
+MovementController& movementController = MovementController::GetInstance();
+
+void setup_movement() {
+  movementController.AttachAzimuthMotor(10, 11, 4000 / TWO_PI);
+  movementController.AttachAltitudeMotor(12, 13, 4000 / TWO_PI);
+}
+
+void btnm_jog_value_changed_handler(lv_event_t* e) {
+  lv_obj_t* btnm = reinterpret_cast<lv_obj_t*>(lv_event_get_target(e));
+  int32_t id = lv_buttonmatrix_get_selected_button(btnm);
+
+  switch (id) {
+    case 1:  // Up
+      movementController.JogUp();
+      break;
+    case 3:  // Left
+      movementController.JogEast();
+      break;
+    case 4:  // OK
+      // Implement OK action if needed
+      debugln("OK button pressed");
+      break;
+    case 5:  // Right
+      movementController.JogWest();
+      break;
+    case 7:  // Down
+      movementController.JogDown();
+      break;
+    default:
+      break;
+  }
+}
 
 void setup(void) {
   Serial.begin(9600);
   delay(1000);  // delay for the serial monitor to open
+  setup_movement();
+
   setup_screen();
+  lv_obj_t* tabview = lv_tabview_create(lv_screen_active());
+  lv_obj_t* tab_goto = lv_tabview_add_tab(tabview, "Goto");
+  lv_obj_t* tab_identify = lv_tabview_add_tab(tabview, "Identify");
+
+  populate_tab_identify(tab_identify, btnm_jog_value_changed_handler);
 }
 
 void loop(void) {
   lv_task_handler();
+  movementController.Run();
   lv_tick_inc(5);
   delay(5);
 }
