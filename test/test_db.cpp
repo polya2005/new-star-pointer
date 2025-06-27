985e1d433089af2636816d6dd6f3ea0d2d9d83e3
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-06-27 16:38:25 +0700

    initial commit

---
+++ test/test_db.cpp
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2025, Boonyakorn Thanpanit
+ */
+
+#include <Arduino.h>
+#include <StarDatabase.h>
+#include <unity.h>
+
+static StarDatabase& star_database = StarDatabase::GetInstance();
+
+void setUp(void) {
+  star_database.Init();
+}
+
+void test_test(void) { TEST_ASSERT_TRUE(true); }
+
+void test_search_by_position(void) {
+  double ra = 1.373;
+  double dec = -0.14292;
+  int16_t index = star_database.SearchByPosition(ra, dec);
+  TEST_ASSERT_EQUAL(index, 705);
+}
+
+void setup() {
+  delay(2000);
+  UNITY_BEGIN();
+  RUN_TEST(test_test);
+  // RUN_TEST(test_search_by_position);
+  UNITY_END();
+}
+
+void loop() {}
