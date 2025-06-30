d710dc9f337e1afed53bf256e7ce3aaedda87da2
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-06-30 19:23:58 +0700

    add create_star_info_screen

--- include/ui.h
+++ include/ui.h
@@ -17,4 +17,18 @@ void setup_screen();
 lv_obj_t* populate_tab_identify(lv_obj_t* tab_identify,
                                 lv_event_cb_t event_handler);
 
+/**
+ * @brief Creates a screen displaying a star's information.
+ * 
+ * This function creates a screen that displays detailed information about a star,
+ * including its name, right ascension, declination, magnitude, spectral type,
+ * constellation, and B-V color index. It also includes a button to return to the
+ * previous screen.
+ * 
+ * @param star_index The index of the star in the database.
+ * 
+ * @return lv_obj_t* A pointer to the created screen object.
+ */
+lv_obj_t* create_star_info_screen(int16_t star_index);
+
 #endif  // UI_H_
