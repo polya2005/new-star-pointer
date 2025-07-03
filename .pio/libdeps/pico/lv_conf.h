3ac944ba1d77cff1fee890a2cb385a458fd73def
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-07-03 17:00:07 +0700

    add and use notosans font

--- .pio/libdeps/pico/lv_conf.h
+++ .pio/libdeps/pico/lv_conf.h
@@ -483,7 +483,7 @@
  *https://fonts.google.com/specimen/Montserrat*/
 #define LV_FONT_MONTSERRAT_8  0
 #define LV_FONT_MONTSERRAT_10 0
-#define LV_FONT_MONTSERRAT_12 0
+#define LV_FONT_MONTSERRAT_12 1
 #define LV_FONT_MONTSERRAT_14 1
 #define LV_FONT_MONTSERRAT_16 0
 #define LV_FONT_MONTSERRAT_18 0
@@ -516,10 +516,10 @@
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
  *E.g. #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)*/
-#define LV_FONT_CUSTOM_DECLARE
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(notosans_12) LV_FONT_DECLARE(notosans_14)
 
 /*Always set a default font*/
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_DEFAULT &notosans_14
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
---
+++ src/ui/create_star_info_screen.cpp
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2025, Boonyakorn Thanpanit
+ */
+
+#include <StarDatabase.h>
+#include <lvgl.h>
+#include <stdio.h>
+
+#include "ui.h"
+
+
+const char* make_ra_string(double ra) {
+  // Convert right ascension from radians to hours
+  double hours = ra * 12.0 / PI;
+  int h = static_cast<int>(hours);
+  int m = static_cast<int>((hours - h) * 60);
+  double s = ((hours - h) * 60 - m) * 60;
+
+  static char ra_string[17];
+  snprintf(ra_string, sizeof(ra_string), "%02dʰ%02dᵐ%4.1fˢ", h, m, s);
+  return ra_string;
+}
+
+const char* make_dec_string(double dec) {
+  // Convert declination from radians to degrees
+  double degrees = dec * 180.0 / PI;
+  char sign;
+  if (degrees < 0) {
+    sign = '-';
+    degrees = -degrees;  // Make it positive for formatting
+  } else {
+    sign = '+';
+  }
+  int d = static_cast<int>(degrees);
+  int m = static_cast<int>((degrees - d) * 60);
+  int s = static_cast<int>(((degrees - d) * 60 - m) * 60);
+
+  static char dec_string[12];
+  snprintf(dec_string, sizeof(dec_string), "%c%02d°%02d'%02d\"", sign, d, m, s);
+  return dec_string;
+}
+
+lv_obj_t* create_star_info_screen(int16_t star_index) {
+  StarDatabase& star_database = StarDatabase::GetInstance();
+  StarDatabaseEntry star_entry = star_database.ReadStarEntry(star_index);
+
+  // Create a new screen for displaying star information
+  lv_obj_t* screen = lv_obj_create(NULL);
+  lv_obj_set_layout(screen, LV_LAYOUT_FLEX);
+  lv_obj_set_flex_flow(screen, LV_FLEX_FLOW_COLUMN);
+  lv_obj_set_flex_align(screen, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER,
+                        LV_FLEX_ALIGN_SPACE_AROUND);
+
+  // Create a label for the star name
+  lv_obj_t* title = lv_label_create(screen);
+  lv_label_set_text(title, star_entry.names[0]);
+
+  // Create a table for displaying star information
+  lv_obj_t* table = lv_table_create(screen);
+  lv_table_set_column_count(table, 2);
+  lv_obj_set_style_text_font(table, &notosans_12, LV_PART_ITEMS);
+  lv_obj_set_style_pad_all(table, 4, LV_PART_ITEMS);
+
+  lv_table_set_cell_value(table, 0, 0, "Other Names");
+  lv_table_set_cell_value(table, 0, 1, star_entry.names[1]);
+
+  lv_table_set_cell_value(table, 1, 0, "Constellation");
+  lv_table_set_cell_value(table, 1, 1, star_entry.constellation);
+
+  lv_table_set_cell_value(table, 2, 0, "Right Ascension");
+  lv_table_set_cell_value(table, 2, 1, make_ra_string(star_entry.ra));
+
+  lv_table_set_cell_value(table, 3, 0, "Declination");
+  lv_table_set_cell_value(table, 3, 1, make_dec_string(star_entry.dec));
+
+  lv_table_set_cell_value(table, 4, 0, "Magnitude");
+  char mag_str[6];
+  snprintf(mag_str, sizeof(mag_str), "%.2f", star_entry.mag);
+  lv_table_set_cell_value(table, 4, 1, mag_str);
+
+  lv_table_set_cell_value(table, 5, 0, "Spectral Type");
+  lv_table_set_cell_value(table, 5, 1, star_entry.spectral_type);
+
+  lv_table_set_cell_value(table, 6, 0, "B-V Color Index");
+  char bv_str[6];
+  snprintf(bv_str, sizeof(bv_str), "%.2f", star_entry.b_v);
+  lv_table_set_cell_value(table, 6, 1, bv_str);
+
+  return screen;
+}
